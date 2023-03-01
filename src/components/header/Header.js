f524890f1fa57350b7b3e29689b5d095560a7d08
LuongThienPhuoc <thienphuoc19032001@gmail.com>
2023-03-01 14:26:19 +0700

    done login

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,50 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Menu from './Menu'
+// import Menu from './Menu'
 import Search from './Search'
+import { Image } from 'antd'
+import Logo from "../../images/logo.jpg"
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer } = Layout;
 
-const Header = () => {
+const HeaderLayout = () => {
     return (
-        <div className="header bg-light">
-            <nav className="navbar navbar-expand-lg navbar-light 
-            bg-light justify-content-between align-middle">
+        // <div className="header bg-light">
+        //     <nav className="navbar navbar-expand-lg navbar-light 
+        //     bg-light justify-content-between align-middle">
 
-                <Link to="/" className="logo">
-                    <h1 className="navbar-brand text-uppercase p-0 m-0"
-                        onClick={() => window.scrollTo({ top: 0 })}>
-                        V-Network
-                    </h1>
-                </Link>
+        //         <Link to="/" className="logo">
+        //             <h1 className="navbar-brand text-uppercase p-0 m-0"
+        //                 onClick={() => window.scrollTo({ top: 0 })}>
+        //                 <Image
+        //                     width={50}
+        //                     src={Logo}
+        //                     preview={false}
+        //                 />
+        //             </h1>
+        //         </Link>
 
-                <Search />
+        //         <Search />
 
-                <Menu />
-            </nav>
-        </div>
+        //         <Menu />
+        //     </nav>
+        // </div>
+        <Header>
+            <div className="logo" />
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}
+                items={new Array(15).fill(null).map((_, index) => {
+                    const key = index + 1;
+                    return {
+                        key,
+                        label: `nav ${key}`,
+                    };
+                })}
+            />
+        </Header>
     )
 }
 
-export default Header
+export default HeaderLayout
--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -1,62 +1,52 @@
 import { GLOBALTYPES } from './globalTypes'
 import { postDataAPI } from '../../utils/fetchData'
 import valid from '../../utils/valid'
-
+import { message } from 'antd'
 
 export const login = (data) => async (dispatch) => {
     try {
-        dispatch({ type: GLOBALTYPES.ALERT, payload: {loading: true} })
+        dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } })
         const res = await postDataAPI('login', data)
-        dispatch({ 
-            type: GLOBALTYPES.AUTH, 
+        dispatch({
+            type: GLOBALTYPES.AUTH,
             payload: {
                 token: res.data.access_token,
                 user: res.data.user
-            } 
+            }
         })
 
         localStorage.setItem("firstLogin", true)
-        dispatch({ 
-            type: GLOBALTYPES.ALERT, 
-            payload: {
-                success: res.data.msg
-            } 
-        })
-        
+        message.success(res.data.msg)
     } catch (err) {
-        dispatch({ 
-            type: GLOBALTYPES.ALERT, 
-            payload: {
-                error: err.response.data.msg
-            } 
-        })
+        message.error(err.response.data.msg)
     }
-}
+    dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: false } })
 
+}
 
 export const refreshToken = () => async (dispatch) => {
     const firstLogin = localStorage.getItem("firstLogin")
-    if(firstLogin){
-        dispatch({ type: GLOBALTYPES.ALERT, payload: {loading: true} })
+    if (firstLogin) {
+        dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } })
 
         try {
             const res = await postDataAPI('refresh_token')
-            dispatch({ 
-                type: GLOBALTYPES.AUTH, 
+            dispatch({
+                type: GLOBALTYPES.AUTH,
                 payload: {
                     token: res.data.access_token,
                     user: res.data.user
-                } 
+                }
             })
 
             dispatch({ type: GLOBALTYPES.ALERT, payload: {} })
 
         } catch (err) {
-            dispatch({ 
-                type: GLOBALTYPES.ALERT, 
+            dispatch({
+                type: GLOBALTYPES.ALERT,
                 payload: {
                     error: err.response.data.msg
-                } 
+                }
             })
         }
     }
@@ -64,38 +54,37 @@ export const refreshToken = () => async (dispatch) => {
 
 export const register = (data) => async (dispatch) => {
     const check = valid(data)
-    if(check.errLength > 0)
-    return dispatch({type: GLOBALTYPES.ALERT, payload: check.errMsg})
+    if (check.errLength > 0)
+        return dispatch({ type: GLOBALTYPES.ALERT, payload: check.errMsg })
 
     try {
-        dispatch({type: GLOBALTYPES.ALERT, payload: {loading: true}})
+        dispatch({ type: GLOBALTYPES.ALERT, payload: { loading: true } })
 
         const res = await postDataAPI('register', data)
-        dispatch({ 
-            type: GLOBALTYPES.AUTH, 
+        dispatch({
+            type: GLOBALTYPES.AUTH,
             payload: {
                 token: res.data.access_token,
                 user: res.data.user
-            } 
+            }
         })
 
         localStorage.setItem("firstLogin", true)
-        dispatch({ 
-            type: GLOBALTYPES.ALERT, 
+        dispatch({
+            type: GLOBALTYPES.ALERT,
             payload: {
                 success: res.data.msg
-            } 
+            }
         })
     } catch (err) {
-        dispatch({ 
-            type: GLOBALTYPES.ALERT, 
+        dispatch({
+            type: GLOBALTYPES.ALERT,
             payload: {
                 error: err.response.data.msg
-            } 
+            }
         })
     }
 }
-
 
 export const logout = () => async (dispatch) => {
     try {
@@ -103,11 +92,11 @@ export const logout = () => async (dispatch) => {
         await postDataAPI('logout')
         window.location.href = "/"
     } catch (err) {
-        dispatch({ 
-            type: GLOBALTYPES.ALERT, 
+        dispatch({
+            type: GLOBALTYPES.ALERT,
             payload: {
                 error: err.response.data.msg
-            } 
+            }
         })
     }
 }
