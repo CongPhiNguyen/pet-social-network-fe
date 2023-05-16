fb02433321fd792335652a6edde1d744a8c78680
CongPhiNguyen <congphinguyen312@gmail.com>
2023-05-17 01:05:27 +0700

    fix api end point

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API_ENDPOINT =
-  "https://pet-social-be.onrender.com" || "http://localhost:5000/api/"
+  "https://pet-social-be.onrender.com/api" || "http://localhost:5000/api/"
 
 const instance = axios.create({
   baseURL: API_ENDPOINT
