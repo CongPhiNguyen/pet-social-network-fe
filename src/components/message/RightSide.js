65581e46adedd26acf07b8488d98c638e5c7a754
LuongThienPhuoc <thienphuoc19032001@gmail.com>
2023-05-16 22:26:39 +0700

    call

--- src/components/message/RightSide.js
+++ src/components/message/RightSide.js
@@ -14,7 +14,7 @@ import {
   deleteConversation
 } from "../../redux/actions/messageAction"
 import LoadIcon from "../../images/loading.gif"
-import { Button } from "antd"
+import { Button, Spin } from "antd"
 import { ExclamationCircleFilled } from '@ant-design/icons';
 import { Modal } from 'antd'
 
@@ -260,8 +260,8 @@ const RightSide = () => {
           ))}
 
           {loadMedia && (
-            <div className="chat_row you_message">
-              <img src={LoadIcon} alt="loading" />
+            <div style={{ display: "flex", justifyContent: "end" }}>
+              <Spin></Spin>
             </div>
           )}
         </div>
