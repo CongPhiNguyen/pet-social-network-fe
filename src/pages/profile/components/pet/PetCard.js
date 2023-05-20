bc8d36f637afe89ad2bbe213f5713da7c0e83697
CongPhiNguyen <congphinguyen312@gmail.com>
2023-05-20 19:07:17 +0700

    fix warning

--- src/pages/profile/components/pet/PetCard.js
+++ src/pages/profile/components/pet/PetCard.js
@@ -4,21 +4,38 @@ import { AiFillDelete } from "react-icons/ai"
 
 export default function PetCard({ image, name, description, isShowDelete }) {
   return (
-    <div style={{ textAlign: "center", position: "relative" }}>
-      <img
-        src={image}
-        style={{ width: 80, height: 80, marginBottom: 10, borderRadius: 10 }}
-        alt={`${name}`}
-      />
+    <div style={{ textAlign: "center", width: "100%" }}>
+      <div
+        style={{
+          position: "relative",
+          width: 80,
+          height: 80,
+          margin: "auto",
+          textAlign: "center",
+          marginBottom: 10
+        }}
+      >
+        <img
+          src={image}
+          style={{
+            width: "100%",
+            height: "100%",
+            marginBottom: 10,
+            borderRadius: 10
+          }}
+          alt={`${name}`}
+        />
+
+        {false && (
+          <AiFillDelete
+            style={{ position: "absolute", top: -10, right: -10 }}
+            color="red"
+            size={20}
+          />
+        )}
+      </div>
       <Typography style={{ fontWeight: 600 }}>{name}</Typography>
       <p>{description}</p>
-      {isShowDelete && (
-        <AiFillDelete
-          style={{ position: "absolute", top: -10, right: 12 }}
-          color="red"
-          size={20}
-        />
-      )}
     </div>
   )
 }
