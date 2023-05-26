41fd0425831f8aeedef3d52334ede89318d6849f
CongPhiNguyen <congphinguyen312@gmail.com>
2023-05-27 02:21:51 +0700

    basic 404 page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,53 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Menu from './Menu'
-import Search from './Search'
-import { Image } from 'antd'
+import React from "react"
+import { Link } from "react-router-dom"
+import Menu from "./Menu"
+import Search from "./Search"
+import { Image } from "antd"
 import Logo from "../../images/logo.jpg"
-import { Breadcrumb, Layout, theme, Row, Col } from 'antd';
-const { Header } = Layout;
+import { Breadcrumb, Layout, theme, Row, Col } from "antd"
+const { Header } = Layout
 
 const HeaderLayout = () => {
-    return (
-        <Header style={{ display: "flex", justifyContent: "center", position: "fixed", top: 0, left: 0, right: 0, zIndex: 1029 }}>
-            <Row style={{ maxWidth: "1200px", width: "100%" }} >
-                <Col xs={2} >
-                    <Link to="/" className="logo">
-                        <Image
-                            className='header-logo'
-                            width={50}
-                            src={Logo}
-                            preview={false}
-                            onClick={() => window.scrollTo({ top: 0 })}
-                        />
-                    </Link>
-                </Col>
-                <Col style={{ display: "flex", justifyContent: "center", alignItems: "center" }} xs={10} >
-                    <Search />
-                </Col>
-                <Col xs={12} >
-                    <Menu />
-                </Col>
-            </Row>
-        </Header>
-    )
+  return (
+    <Header
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 1029
+      }}
+    >
+      <Row style={{ maxWidth: "1200px", width: "100%" }}>
+        <Col xs={2}>
+          <Link to="/" className="logo">
+            <Image
+              className="header-logo"
+              width={50}
+              src={Logo}
+              preview={false}
+              onClick={() => window.scrollTo({ top: 0 })}
+            />
+          </Link>
+        </Col>
+        <Col
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+          xs={10}
+        >
+          <Search />
+        </Col>
+        <Col xs={12}>
+          <Menu />
+        </Col>
+      </Row>
+    </Header>
+  )
 }
 
 export default HeaderLayout
--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -1,16 +1,41 @@
 import React from "react"
-
+import BackGround from "../assets/images/404.png"
+import { Button, Image, Row } from "antd"
+import { useNavigate } from "react-router-dom"
 export default function NotFoundPage() {
+  const navigate = useNavigate()
   return (
     <div
       style={{
         backgroundImage: "url()"
       }}
     >
-      <img
-        src="https://res.cloudinary.com/databaseimg/image/upload/v1684019590/hvxtfottpjl9jsy2ba74.jpg"
-        alt="404"
-      />
+      <div style={{ textAlign: "center", marginTop: 100 }}>
+        <img src={BackGround} alt="404 cat" style={{ width: "40%" }} />
+        <p>
+          You do not have permission to access this page or this page does not
+          exist
+        </p>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 4,
+            alignItems: "center"
+          }}
+        >
+          <Button type="primary">Back</Button>
+          <Button
+            type="primary"
+            onClick={() => {
+              navigate("/")
+            }}
+          >
+            Go to main page
+          </Button>
+          <Button type="">Report to admin</Button>
+        </div>
+      </div>
     </div>
   )
 }
