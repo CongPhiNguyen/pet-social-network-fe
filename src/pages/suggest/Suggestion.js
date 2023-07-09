7500a1e1460f813852a90d871a0d0ca1a70d4814
CongPhiNguyen <congphinguyen312@gmail.com>
2023-07-09 19:10:15 +0700

    basic display and call rec api ok

--- src/pages/suggest/Suggestion.js
+++ src/pages/suggest/Suggestion.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react"
 import { useEffect } from "react"
 import { getSuggestionsApi } from "../../api/user"
+import { useSelector } from "react-redux"
 
 export default function Suggestion() {
   const [suggestions, setSuggestion] = useState([])
+  const { user } = useSelector((state) => state.auth)
+  // console.log(user._id)
   const getSuggestion = async () => {
-    const response = await getSuggestionsApi()
+    const response = await getSuggestionsApi(user._id)
     const { data, status } = response
     if (status === 200) {
       const suggestion = data.suggestion
